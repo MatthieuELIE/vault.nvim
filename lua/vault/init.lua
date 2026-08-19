35deed2614fe5053701324422e32ab168d5ce8bd
Matthieu ELIE <elie.matthieu2@gmail.com>
2026-08-19 09:02:39 +0200

    feat(core): make global keymaps opt-in via opts.keys

--- lua/vault/init.lua
+++ lua/vault/init.lua
@@ -21,46 +21,53 @@ M.setup = function(opts)
         M.search(o.args)
     end, { force = true, nargs = '?' })
 
-    local keys = vim.tbl_extend('force', {
-        toggle_todo = '<leader>vt',
-        quick_add_todo = '<leader>vi',
-        toggle_checkbox = '<leader>vc',
-        toggle_diary = '<leader>vd',
-        diary_next = '<leader>vn',
-        diary_prev = '<leader>vp',
-        diary_goto = '<leader>vg',
-        search = '<leader>vs',
-        archive_todos = '<leader>va',
-    }, opts.keys or {})
+    local key_overrides = opts.keys == true and {} or (opts.keys or {})
 
-    local keymap_specs = {
-        { name = 'toggle_todo', fn = M.toggle_todo, desc = 'Toggle project todo' },
-        { name = 'quick_add_todo', fn = M.quick_add_todo, desc = 'Quick add a project todo' },
-        { name = 'toggle_diary', fn = M.toggle_diary, desc = 'Toggle today diary' },
-        { name = 'diary_next', fn = M.diary_next_day, desc = 'Go to next diary day' },
-        { name = 'diary_prev', fn = M.diary_prev_day, desc = 'Go to previous diary day' },
-        { name = 'diary_goto', fn = M.diary_goto, desc = 'Go to a diary date' },
-        { name = 'search', fn = M.search, desc = 'Search the vault' },
-    }
-    for _, spec in ipairs(keymap_specs) do
-        if keys[spec.name] then
-            vim.keymap.set('n', keys[spec.name], spec.fn, { noremap = true, desc = spec.desc })
+    if opts.keys then
+        local global_keys = vim.tbl_extend('force', {
+            toggle_todo = '<leader>vt',
+            quick_add_todo = '<leader>vi',
+            toggle_diary = '<leader>vd',
+            diary_next = '<leader>vn',
+            diary_prev = '<leader>vp',
+            diary_goto = '<leader>vg',
+            search = '<leader>vs',
+        }, key_overrides)
+
+        local global_keymap_specs = {
+            { name = 'toggle_todo', fn = M.toggle_todo, desc = 'Toggle project todo' },
+            { name = 'quick_add_todo', fn = M.quick_add_todo, desc = 'Quick add a project todo' },
+            { name = 'toggle_diary', fn = M.toggle_diary, desc = 'Toggle today diary' },
+            { name = 'diary_next', fn = M.diary_next_day, desc = 'Go to next diary day' },
+            { name = 'diary_prev', fn = M.diary_prev_day, desc = 'Go to previous diary day' },
+            { name = 'diary_goto', fn = M.diary_goto, desc = 'Go to a diary date' },
+            { name = 'search', fn = M.search, desc = 'Search the vault' },
+        }
+        for _, spec in ipairs(global_keymap_specs) do
+            if global_keys[spec.name] then
+                vim.keymap.set('n', global_keys[spec.name], spec.fn, { noremap = true, desc = spec.desc })
+            end
         end
     end
+
+    local buffer_local_keys = vim.tbl_extend('force', {
+        toggle_checkbox = '<leader>vc',
+        archive_todos = '<leader>va',
+    }, key_overrides)
 
     local buffer_local_keymap_specs = {
         { name = 'toggle_checkbox', fn = M.toggle_checkbox, desc = 'Toggle markdown checkbox' },
         { name = 'archive_todos', fn = M.archive_todos, desc = 'Archive checked todos' },
     }
-    if keys.toggle_checkbox or keys.archive_todos then
+    if buffer_local_keys.toggle_checkbox or buffer_local_keys.archive_todos then
         vim.api.nvim_create_autocmd('BufEnter', {
             pattern = config.state.vault_path .. '/*/todos.md',
             callback = function(args)
                 for _, spec in ipairs(buffer_local_keymap_specs) do
-                    if keys[spec.name] then
+                    if buffer_local_keys[spec.name] then
                         vim.keymap.set(
                             'n',
-                            keys[spec.name],
+                            buffer_local_keys[spec.name],
                             spec.fn,
                             { noremap = true, buffer = args.buf, desc = spec.desc }
                         )
